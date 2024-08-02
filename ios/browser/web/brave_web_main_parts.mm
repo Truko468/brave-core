b32275a367c60fe3b8a85eeec2472d40daa14219
Kyle Hickinson <git@kylehickinson.com>
2024-08-02 17:25:35 -0400

    restore universal link block feature

--- ios/browser/web/brave_web_main_parts.mm
+++ ios/browser/web/brave_web_main_parts.mm
@@ -3,31 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/ios/browser/brave_web_main_parts.h"
+#include "brave/ios/browser/web/brave_web_main_parts.h"
 
-#include "base/metrics/user_metrics.h"
+#include "base/command_line.h"
 #include "base/path_service.h"
-#include "base/strings/sys_string_conversions.h"
-#include "base/task/sequenced_task_runner.h"
-#include "base/task/thread_pool.h"
 #include "brave/ios/browser/application_context/brave_application_context_impl.h"
-#include "brave/ios/browser/browser_state/brave_browser_state_keyed_service_factories.h"
-#include "components/flags_ui/pref_service_flags_storage.h"
-#include "components/metrics/metrics_service.h"
-#include "components/metrics_services_manager/metrics_services_manager.h"
-#include "components/variations/service/variations_service.h"
-#include "components/variations/synthetic_trial_registry.h"
-#include "components/variations/synthetic_trials_active_group_id_provider.h"
-#include "components/variations/variations_ids_provider.h"
-#include "components/variations/variations_switches.h"
 #include "ios/chrome/browser/application_context/model/application_context_impl.h"
-#include "ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.h"
-#include "ios/chrome/browser/flags/about_flags.h"
-#include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
-#include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
 #include "ios/chrome/browser/shared/model/paths/paths.h"
-#include "ios/web/public/thread/web_task_traits.h"
-#include "ios/web/public/thread/web_thread.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 #include "ui/base/resource/resource_bundle.h"
 
