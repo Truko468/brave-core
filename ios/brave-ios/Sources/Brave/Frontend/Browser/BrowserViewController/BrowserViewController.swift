d9b4a17af459d847b3fa27a9f9f0605331fd06c8
Kyle Hickinson <git@kylehickinson.com>
2024-08-07 11:19:06 -0400

    use chromium error pages

--- ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BrowserViewController.swift
+++ ios/brave-ios/Sources/Brave/Frontend/Browser/BrowserViewController/BrowserViewController.swift
@@ -2607,7 +2607,6 @@ extension BrowserViewController: TabDelegate {
 
     var injectedScripts: [TabContentScript] = [
       ReaderModeScriptHandler(tab: tab),
-      ErrorPageHelper(certStore: profile.certStore),
       SessionRestoreScriptHandler(tab: tab),
       BlockedDomainScriptHandler(tab: tab),
       HTTPBlockedScriptHandler(tab: tab, exceptionService: braveCore.httpsUpgradeExceptionsService),
@@ -3539,18 +3538,6 @@ extension BrowserViewController {
     guard let tab = tabManager.selectedTab, let webView = tab.webView else {
       Logger.module.error("Invalid WebView")
       return
-    }
-
-    let getServerTrustForErrorPage = { () -> SecTrust? in
-      do {
-        if let url = webView.lastCommittedURL {
-          return try ErrorPageHelper.serverTrust(from: url)
-        }
-      } catch {
-        Logger.module.error("\(error.localizedDescription)")
-      }
-
-      return nil
     }
 
     Task.detached {
